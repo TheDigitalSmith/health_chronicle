a19c212ca425d00223d799765be8dab5b184b7af
infinityshades <tash.aneiq@gmail.com>
2020-06-25 00:31:57 +0200

    user validators

--- src/Model/User/index.js
+++ src/Model/User/index.js
@@ -1,18 +1,46 @@
 const mongoose = require("mongoose");
 const { diseaseSchema } = require("../Diseases");
 const { medicineSchema } = require("../Medicines");
+const Joi = require("@hapi/joi");
 const plm = require("passport-local-mongoose");
 
+const emergencySchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    maxlength: 50,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    max: 50,
+    required: true,
+  },
+  country: {
+    type: String,
+  },
+});
+
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
+  firstName: { type: String, max: 50 },
+  lastName: { type: String, max: 50 },
+  email: { type: String, max: 50 },
   dateOfBirth: Date,
-  sex: String,
-  height: Number,
-  weight: Number,
-  bloodType: String,
-  emergencyContacts: String,
+  sex: { type: String, enum: ["male", "female"] },
+  height: { type: Number, max: 500 },
+  weight: { type: Number, max: 500 },
+  bloodType: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+  },
+  emergencyContacts: [emergencySchema],
   diseases: [{ type: mongoose.Schema.Types.ObjectId, ref: "disease" }],
   medicines: [{ type: mongoose.Schema.Types.ObjectId, ref: "medicine" }],
 });
@@ -21,4 +49,21 @@ userSchema.plugin(plm);
 
 const User = mongoose.model("user", userSchema);
 
-module.exports = User;
+function userValidator(user) {
+  const enumSex = ["male", "female"];
+  const enumBloodType = ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"];
+  const schema = Joi.object({
+    firstName: Joi.string().max(30),
+    lastName: Joi.string().max(30),
+    email: Joi.string().email(),
+    dateOfBirth: Joi.date(),
+    sex: Joi.string.valid(...enumSex),
+    height: Joi.number().max(500),
+    weight: Joi.number().max(500),
+    bloodType: Joi.string.valid(...enumBloodType),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports = { User, userValidator };
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const userRouter = express.Router();
 const passport = require("passport");
 
-const User = require("../../Model/User");
+const { User, userValidator } = require("../../Model/User");
 const { Disease } = require("../../Model/Diseases");
 const { Medicine } = require("../../Model/Medicines");
 const { getToken } = require("../../utils/auth");
+const validate = require("../../utils/validate");
 
 userRouter.get("/me", passport.authenticate("jwt"), async (req, res) => {
   const user = await User.findById(req.user._id).populate("diseases");
@@ -25,93 +26,113 @@ userRouter.post("/signin", passport.authenticate("local"), async (req, res) => {
   res.send({ user, access_token: token });
 });
 
-userRouter.put("/me", passport.authenticate("jwt"), async (req, res) => {
-  const updatedInfo = await User.findByIdAndUpdate(
-    req.user._id,
-    { $set: { ...req.body } },
-    { new: true, runValidators: true }
-  );
-  res.send(updatedInfo);
-});
+userRouter.put(
+  "/me",
+  [passport.authenticate("jwt"), validate(userValidator)],
+  async (req, res) => {
+    const updatedInfo = await User.findByIdAndUpdate(
+      req.user._id,
+      { $set: { ...req.body } },
+      { new: true, runValidators: true }
+    );
+    res.send(updatedInfo);
+  }
+);
 
 userRouter.delete("/", passport.authenticate("jwt"), async (req, res) => {
   const removeUser = await User.findByIdAndRemove(req.user._id);
   res.send({ Remove: "Successful", user: removeUser });
 });
 
-userRouter.put("/diseases", passport.authenticate("jwt"), async (req, res) => {
-  const disease = await Disease.findOne({ name: req.body.name });
-  if (!disease) {
-    const newDisease = await Disease.create(req.body);
+userRouter.put(
+  "/diseases/add",
+  passport.authenticate("jwt"),
+  async (req, res) => {
+    const disease = await Disease.findOne({ name: req.body.name });
+    if (!disease) {
+      const newDisease = await Disease.create(req.body);
+      const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $push: {
+            diseases: newDisease._id,
+          },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.json(user);
+    }
     const user = await User.findByIdAndUpdate(
       req.user._id,
       {
         $push: {
-          diseases: newDisease._id,
+          diseases: disease._id,
         },
       },
       { new: true, runValidators: true }
     );
-    return res.json(user);
+    res.json(user);
   }
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $push: {
-        diseases: disease._id,
-      },
-    },
-    { new: true, runValidators: true }
-  );
-  res.json(user);
-});
+);
 
-userRouter.put("/diseases", passport.authenticate("jwt"), async (req, res) => {
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $pull: {
-        diseases: req.body.diseaseID,
-      },
-    },
-    { new: true, runValidators: true }
-  );
-  if (!user) return res.status(404).json("User/disease not found");
-  res.json(user);
-});
-
-userRouter.put("/medicines", passport.authenticate("jwt"), async (req, res) => {
-  const medicine = await Medicine.findOne({ name: req.body.name });
-  if (!medicine) {
-    const newMedicine = await Medicine.create(req.body);
+userRouter.put(
+  "/diseases/remove",
+  passport.authenticate("jwt"),
+  async (req, res) => {
     const user = await User.findByIdAndUpdate(
       req.user._id,
       {
-        $push: { medicines: newMedicine._id },
+        $pull: {
+          diseases: req.body.diseaseID,
+        },
       },
       { new: true, runValidators: true }
     );
-    return res.json(user);
+    if (!user) return res.status(404).json("User/disease not found");
+    res.json(user);
   }
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $push: { medicines: medicine._id },
-    },
-    { new: true, runValidators: true }
-  );
-  res.json(user);
-});
+);
 
-userRouter.put("/medicine", passport.authenticate("jwt"), async (req, res) => {
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $pull: { medicine: req.body.medicineID },
-    },
-    { new: true, runValidators: true }
-  );
-  if (!user) return res.status(404).json("User/disease not found");
-  res.json(user);
-});
+userRouter.put(
+  "/medicines/add",
+  passport.authenticate("jwt"),
+  async (req, res) => {
+    const medicine = await Medicine.findOne({ name: req.body.name });
+    if (!medicine) {
+      const newMedicine = await Medicine.create(req.body);
+      const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $push: { medicines: newMedicine._id },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.json(user);
+    }
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $push: { medicines: medicine._id },
+      },
+      { new: true, runValidators: true }
+    );
+    res.json(user);
+  }
+);
+
+userRouter.put(
+  "/medicine/remove",
+  passport.authenticate("jwt"),
+  async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $pull: { medicine: req.body.medicineID },
+      },
+      { new: true, runValidators: true }
+    );
+    if (!user) return res.status(404).json("User/disease not found");
+    res.json(user);
+  }
+);
 module.exports = userRouter;
