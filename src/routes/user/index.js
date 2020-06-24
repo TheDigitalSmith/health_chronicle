7fa65d305035e9ca48d6717eb2f95915743eb926
infinityshades <tash.aneiq@gmail.com>
2020-06-24 17:15:34 +0200

    diseaseRoute

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const User = require("../../Model/User");
 const userRouter = express.Router();
-const { getToken } = require("../../utils/auth");
 const passport = require("passport");
 
+const User = require("../../Model/User");
+const { Disease } = require("../../Model/Diseases");
+const { getToken } = require("../../utils/auth");
+
 userRouter.get("/me", passport.authenticate("jwt"), async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id).populate("diseases");
   res.send(user);
 });
 
@@ -35,5 +37,44 @@ userRouter.delete("/", passport.authenticate("jwt"), async (req, res) => {
   const removeUser = await User.findByIdAndRemove(req.user._id);
   res.send({ Remove: "Successful", user: removeUser });
 });
+
+userRouter.put("/diseases", passport.authenticate("jwt"), async (req, res) => {
+  const disease = await Disease.findOne({ name: req.body.name });
+  if (!disease) {
+    const newDisease = await Disease.create(req.body);
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $push: {
+          diseases: newDisease._id,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+    console.log(user);
+    return res.json(user);
+  }
+  const user = await User.findById(req.user._id);
+  user.diseases.push(disease._id);
+  await user.save();
+  res.json(user);
+});
+
+userRouter.delete(
+  "/diseases",
+  passport.authenticate("jwt"),
+  async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $pull: {
+          diseases: req.body.diseaseID,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+    res.json(user);
+  }
+);
 
 module.exports = userRouter;
